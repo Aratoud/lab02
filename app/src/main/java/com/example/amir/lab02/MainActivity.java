02cf60e93ce7df195b6187de8f3efae34983b75f
Amir Hossein <amiraratoud@gmail.com>
2018-02-27 18:20:56 +0300

    yeni123

--- app/src/main/java/com/example/amir/lab02/MainActivity.java
+++ app/src/main/java/com/example/amir/lab02/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
         // check for url extension
         System.out.println( "**************************" + url + "********************************");
-        if ( url.substring( url.length() - 3, url.length()).equals( "txtz")) {
+        if ( url.substring( url.length() - 3, url.length()).equals( "txtz123")) {
             return new MySimpleURLReader( url);
         } else if ( url.substring( url.length() - 3, url.length()).equals( "htm")
                 || url.substring( url.length() - 4, url.length()).equals( "html")) {
